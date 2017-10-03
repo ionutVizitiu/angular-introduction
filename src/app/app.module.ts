8043516c34ad9d1336f695f41353133388ea32fa
ionutVizitiu <ionutVizitiu@users.noreply.github.com>
2017-10-03 21:28:47 +0200

    chore: removed unnecessary spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { MaterialModule } from '@angular/material';
 import { A2appRoutingModule } from './app-routing.module';
-
 import { AlertModule } from 'ng2-bootstrap';
-
 import { AppComponent } from './app.component';
 import { ChildComponent } from './components/child/child.component';
 import { NoContentComponent } from './components/no-content/no-content.component';
